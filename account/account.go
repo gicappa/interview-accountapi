6a7374fbeb057b68807492527d2df9bfdf77b34a
Gian Carlo Pace <giancarlo.pace@gmail.com>
2021-02-28 19:53:48 +0100

    adding the signature of the method Create

--- account/account.go
+++ account/account.go
@@ -2,34 +2,56 @@ package account
 
 import r "github.com/gicappa/interview-accountapi/rest"
 
-// Account holds the account data
-type Account struct {
-	Data struct {
-		Type           string `json:"type"`
-		ID             string `json:"id"`
-		OrganisationID string `json:"organisation_id"`
-		Attributes     struct {
-			Country      string `json:"country"`
-			BaseCurrency string `json:"base_currency"`
-			BankID       string `json:"bank_id"`
-			BankIDCode   string `json:"bank_id_code"`
-			Bic          string `json:"bic"`
-		} `json:"attributes"`
-	} `json:"data"`
-}
-
-// Client is the client that allows to marshal and unmarshal
-// the objects
+// Account
+// =======
+// An Account represents a bank account that is registered
+// with Form3. It is used to validate and allocate inbound
+// payments.
+//
+// Client is the base structure that is able to translate the
+// golang methods into RESTful calls. It holds a rest client
+// structure to ease the translation of the command in HTTP
+// verbs
 type Client struct {
 	rest r.REST
 }
 
-// NewClient creates a new object that will help in
-// translating commmands in code to HTTP requests
+// NewClient is a function that creates a new object obejct
+// injecting the rest client to the DefaultREST client that
+// is actually performing the requests to the API layer.
 func NewClient() *Client {
 	return &Client{
 		rest: &r.DefaultREST{},
 	}
+}
+
+// Create registers an existing bank account with Form3 or create
+// a new one. The country attribute must be specified as
+// a minimum. Depending on the country, other attributes
+// such as bank_id and bic are mandatory.
+//
+// Form3 generates account numbers and IBANs, where
+// appropriate, in the following cases:
+//
+// If no account number or IBAN is provided, Form3 generates
+// a valid account number (see below). If supported by
+// the country, an IBAN is also generated.
+// If an account number is provided but the IBAN is empty,
+// Form3 generates an IBAN if supported by the country.
+// If only an IBAN is provided, the account number will be \
+// left empty.
+// Note that a given bank_id and bic need to be registered
+// with Form3 and connected to your organisation ID.
+func (c *Client) Create(country string, bankID string, bankIDCode string, BIC string) (a *Account, err error) {
+	return &Account{}, nil
+}
+
+// Account is the data structure holding account data
+// coming from the APIs answers
+type Account struct {
+	accountNumber string
+	status        string
+	IBAN          string
 }
 
 // // CreateAccount creates a new account in the API
@@ -37,4 +59,20 @@ func NewClient() *Client {
 // 	c.rest.Post("/v1/organisation/accounts", nil)
 
 // 	return &Account{}, nil
+// }
+
+// Account holds the account data
+// type Account struct {
+// 	Data struct {
+// 		Type           string `json:"type"`
+// 		ID             string `json:"id"`
+// 		OrganisationID string `json:"organisation_id"`
+// 		Attributes     struct {
+// 			Country      string `json:"country"`
+// 			BaseCurrency string `json:"base_currency"`
+// 			BankID       string `json:"bank_id"`
+// 			BankIDCode   string `json:"bank_id_code"`
+// 			Bic          string `json:"bic"`
+// 		} `json:"attributes"`
+// 	} `json:"data"`
 // }
--- account/account_test.go
+++ account/account_test.go
@@ -10,88 +10,44 @@ import (
 var client Client
 
 // Acceptance Tests
-// This is an acceptance test of the client library.
-// It helps with coding by intentions: trying to use the
-// library even before it exists should help me to shape
-// a more usable client library
 //
-// The api should call a POST with a payload similar to
-// this one:
-//
-// POST /v1/organisation/accounts HTTP/1.1
-// Content-Type: application/vnd.api+json
-//
-// {
-//   "data": {
-//     "type": "accounts",
-//     "id": "ad27e265-9605-4b4b-a0e5-3003ea9cc4dc",
-//     "organisation_id": "eb0bd6f5-c3f5-44b2-b677-acd23cdde73c",
-//     "attributes": {
-//       "country": "GB",
-//       "base_currency": "GBP",
-//       "bank_id": "400300",
-//       "bank_id_code": "GBDSC",
-//       "bic": "NWBKGB22"
-//     }
-//   }
-// }
-//
-// and will receive a response similar to the following one:
-// HTTP/1.1 201 Created
-// Content-Type: application/json
-//
-// {
-//   "data": {
-//     "type": "accounts",
-//     "id": "ad27e265-9605-4b4b-a0e5-3003ea9cc4dc",
-//     "version": 0,
-//     "organisation_id": "eb0bd6f5-c3f5-44b2-b677-acd23cdde73c",
-//     "attributes": {
-//       "country": "GB",
-//       "base_currency": "GBP",
-//       "account_number": "41426819",
-//       "bank_id": "400300",
-//       "bank_id_code": "GBDSC",
-//       "bic": "NWBKGB22",
-//       "iban": "GB11NWBK40030041426819",
-//       "status": "confirmed"
-//     },
-//     "relationships": {
-//       "account_events": {
-//         "data": [
-//           {
-//             "type": "account_events",
-//             "id": "c1023677-70ee-417a-9a6a-e211241f1e9c"
-//           },
-//           {
-//             "type": "account_events",
-//             "id": "437284fa-62a6-4f1d-893d-2959c9780288"
-//           }
-//         ]
-//       }
-//     }
-//   }
-// }
-func Test_CreateNewClient(t *testing.T) {
+// The acceptance tests are used to test the actual behavior
+// of the account client libraries connecting to the APIs
+// provided by the docker-compose docker images.
+func TestNewClient(t *testing.T) {
 	client := NewClient()
 
 	assert.NotNil(t, client)
 }
 
+// Create registers an existing bank account with Form3
+// or create a new one
+//
+// Creating a bank account in the UK according to the
+// following constraints:
+// - United Kingdom	Country code: GB
+// - Bank ID: required, 6 characters, UK sort code
+// - BIC: required
+// - Bank ID Code: required, has to be GBDSC
+// - Account Number: optional, 8 characters, generated if not provided
+// - IBAN: Generated if not provided
+func TestCreate_account_GB(t *testing.T) {
+	client := NewClient()
+
+	account, _ := client.Create("GB", "400300", "GBDSC", "NWBKGB22")
+
+	assert.Equal(t, account.status, "confirmed")
+	assert.NotNil(t, account.accountNumber)
+	assert.NotNil(t, account.IBAN)
+}
+
+// Unit tests
 func Test_CreateNewClientWitRESTClient(t *testing.T) {
 	mock := MockREST{}
 	client := Client{rest: &mock}
 
 	assert.NotNil(t, client)
 }
-
-// func Test_ClientCreateAnAccount(t *testing.T) {
-// 	client := NewClient()
-
-// 	actual, _ := client.CreateAccount(&Account{})
-
-// 	assert.Equal(t, actual, "")
-// }
 
 // Micro/Unit tests ///////////////////////////////////////////////////
 // var rest MockREST
